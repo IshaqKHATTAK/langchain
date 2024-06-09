0d9dfe615e8de15f1d4666ee6c0a0da145658267
IshaqKHATTAK <mi5315562@gmail.com>
2024-06-10 04:39:05 +0500

    adding the functioanlity of retrivera and stuff doc chian

--- Assistant.py
+++ Assistant.py
@@ -10,7 +10,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_chroma import Chroma
-from helper import system_template
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from dotenv import load_dotenv
 import os
 
@@ -20,25 +21,25 @@ class LocalAssistant:
         self.assistatn_Llm = None
         self.assistant_Template = None
         self.assistant_Parser = None
+        self.assistant_retriver = None
 
-    def create_Prompt_template(self,system_template = system_template):
+    def create_Prompt_template(self,system_template):
         prompt = ChatPromptTemplate.from_messages(
             [
-                (
-                "system",
-                system_template,
-                ),
-            MessagesPlaceholder(variable_name="messages"),
+                ("system", system_template),
+                ("human", "{input}"),
             ]
         )
         self.assistant_Template = prompt
+        print('prompt created')
         return prompt
 
     def create_LLM(self,llm_name = 'gpt-3.5-turbo'):
         model = ChatOpenAI(model= llm_name)
         self.assistatn_Llm = model
+        print('LLm created')
         return model
-    
+            
     def chat_with_history(self,input,session_id = "abc5"):
         def get_session_history(session_id: str) -> BaseChatMessageHistory:
             if session_id not in LocalAssistant.store:
@@ -54,26 +55,47 @@ class LocalAssistant:
             )
         return response.content
     
-    def text_to_index(self,directory_path):
+    def load_docs(self, directory):
         docs = []
-        for filename in os.listdir(directory_path):
+        for filename in os.listdir(directory):
             if filename.endswith(".pdf"):
-                pdf_path = os.path.join(directory_path, filename)
+                pdf_path = os.path.join(directory, filename)
                 loader = PyPDFLoader(pdf_path)
                 document = loader.load()
                 docs.append(document)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        print('load documents')
+        return docs
+    
+    def split(self,docs,chunk_size = 1000, chunk_overlap = 200):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         for doc in docs:
             splits = text_splitter.split_documents(doc)
+        print('split documents')
+        return splits
     
-        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory="./index")
-    def Retrive_And_Generate(self,input = "what is the main concept behind SSP?"):
-        vectordb = Chroma(persist_directory="./index", embedding_function=OpenAIEmbeddings())
-        retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-        retrieved_docs = retriever.invoke(input)
-        return retrieved_docs[0]
+    def embed_store(self,splits,p_directory):
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=p_directory) 
+        print(f'embed and save to {p_directory}')
+        return vectorstore
+    
+    def text_to_index(self,directory_path,p_directory = "./index", embeding_model = "text-embedding-3-large"):
+        docs = self.load_docs(directory=directory_path)
+        splits = self.split(docs=docs)
+        VS = self.embed_store(splits,p_directory = p_directory)
+        return VS
+       
+    def create_retriver(self,p_directory = "./index",type_search = "similarity",top_k = 5):
+        vectordb = Chroma(persist_directory=p_directory, embedding_function=OpenAIEmbeddings())
+        retriever = vectordb.as_retriever(search_type=type_search, search_kwargs={"k": top_k})
+        self.assistant_retriver = retriever
+        return retriever
+    
+    def create_stuff_and_retrival_chain(self, input,llm,prompt,retriver):
+        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+        rag_chain = create_retrieval_chain(retriver, question_answer_chain)
+        results = rag_chain.invoke({"input": input})
+        return results['answer']
 
-    
     def create_parser(self):
         parser = StrOutputParser()
         self.assistant_Parser = parser
--- MyAssistant.py
+++ MyAssistant.py
@@ -1,21 +1,44 @@
 from Assistant import LocalAssistant
 from dotenv import load_dotenv
 import os
+# You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
 
+# Question: {question} 
+
+# Context: {context} 
+#'''You are a helpful assistant. Answer all questions to the best of your ability.'''
+
+# Answer:
+sys_prompt = ''' "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    conext: {context} '''
 
 load_dotenv()
 api_key =  os.getenv('OPENAI_API')
 os.environ["OPENAI_API_KEY"] = api_key
 
 My_assistant = LocalAssistant()
-My_assistant.create_LLM('gpt-3.5-turbo')
-My_assistant.create_Prompt_template('''You are a helpful assistant. Answer all questions to the best of your ability.''')
+model = My_assistant.create_LLM('gpt-3.5-turbo')
+prompt = My_assistant.create_Prompt_template(sys_prompt)
 
-output = My_assistant.chat_with_history("My name is ishaq")
-print(output)
+My_assistant.text_to_index('./data/',"./index",embeding_model="text-mebedding-3-large")
 
+retriever = My_assistant.create_retriver()
+while True:
+    text = input('Enter a question')
+    if text == 'quit':
+        break
+    output = My_assistant.create_stuff_and_retrival_chain(input= text,llm=model, prompt=prompt, retriver=retriever)
+    print(output)
 
-output = My_assistant.chat_with_history("what is my name")
-print(output)
+# output = My_assistant.chat_with_history("My name is ishaq")
+# print(output)
 
-#print('len of docs', My_assistant.text_to_index('./data/'))
+# output = My_assistant.chat_with_history("what is my name")
+# print(output)
+
+# print('len of docs', My_assistant.text_to_index('./data/'))
